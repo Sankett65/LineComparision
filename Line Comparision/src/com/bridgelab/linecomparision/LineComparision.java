2f9736a8d2608448ce5af39e733fec3d6a57f9c1
Sankett65 <jadhav.jadhav.sanket@gmail.com>
2023-06-18 00:19:37 +0530

    Desplaying Welcome message

---
+++ Line Comparision/src/com/bridgelab/linecomparision/LineComparision.java
@@ -0,0 +1,7 @@
+package com.bridgelab.linecomparision;
+
+public class LineComparision {
+    public static void main(String[] args) {
+        System.out.println("Welcome to the Line Comparison Computation Porgram");
+    }
+}
