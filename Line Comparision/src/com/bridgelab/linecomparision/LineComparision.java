76667b605d61808b06f363fbeb2128d277b387dc
Sankett65 <jadhav.jadhav.sanket@gmail.com>
2023-06-18 14:43:39 +0530

    Checking Equality

--- Line Comparision/src/com/bridgelab/linecomparision/LineComparision.java
+++ Line Comparision/src/com/bridgelab/linecomparision/LineComparision.java
@@ -1,19 +1,34 @@
 package com.bridgelab.linecomparision;
-
+import java.util.Scanner;
+import java.lang.Integer;
 public class LineComparision {
     public static final double LENGTH_X1=0;
     public static final double LENGTH_Y1=0;
-    public static void main(String[] args) {
-        System.out.println("Welcome to the Line Comparison Computation Porgram");
-        double x2=4;
-        double y2=5;
+
+    static void length(int x2,int y2){
         double length1=Math.pow(x2-LENGTH_X1,2);
-       // System.out.println(length1);
         double length2=Math.pow(y2-LENGTH_Y1,2);
-       // System.out.println(length2);
         double length3=length1+length2;
-      //  System.out.println(length3);
-        double length_Of_A_Line=Math.sqrt(length3);
-        System.out.println("Length of a line= " + length_Of_A_Line);
+        double lengthOfALine=Math.sqrt(length3);
+        System.out.println("Length of a line= " + lengthOfALine);
+    }
+
+    static void Equality(double a1, double b1,double c1,double a2, double b2,double c2 ){
+
+        double A =a1/a2;
+        double B= b1/b2;
+        double C= c1/c2;
+        if (A==B && A==C && B==C) {
+            System.out.println("True");
+        }
+
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Welcome to the Line Comparison Computation Porgram");
+        length(3,4);
+        Equality(-2,4,3,-6,12,9);
+
     }
 }
